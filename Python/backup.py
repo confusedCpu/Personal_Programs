9a493cb733227a235582dcdb430d39a1b88c911a
resnet <resnet@RAMS.adp.vcu.edu@saf-alex-linux.rams.adp.vcu.edu>
2018-02-09 11:59:24 -0500

    Backup.py: this script is mostly finished now, just need to add powershell 	   commands for checking the src size and dest size in check_space 	   I also need to implement a way for multiple user profiles to be 	   backed up at once.

--- Python/backup.py
+++ Python/backup.py
@@ -1,46 +1,87 @@
-#!/usr/bin/python3
-# This script will back up the users profile
-# Updated by: martinak Date: 2/1/18
+#!/usr/bin/python3 
 
-from os import access, chdir, getcwd, mkdir 
-from os.path import isdir, islink, join
-from shutil import copytree
-from sys import argv, exit
+'''
+Python3 scipt - backup.py
+This script will back up the users profile 
+This script is intended to be cross platform ( Linux, OSX, Windows ), but has
+not reached the point 
+Updated by: martinak Date: 2/9/18 
+'''
 
-def get_help():
-    print( '''\nThis script will back up user profile information.
-                Two parameters are passed in; the first is the source directory,
-                the second is the destination directory
+from os         import access, listdir, mkdir, popen, stat
+from os.path    import exists, isdir, ismount, join
+from shutil     import copy2
+from sys        import argv, exit
 
-                Usage:
-                    backup.py <src_dir> <dest_dir>
 
-                Example:
-                    backup.py "/mnt/Macintosh HD/Users/John Doe" "/mnt/backup_dir/John Doe"
+def backup( src, dest ):
+# handles the actuall transfer of the files from the source to the destination
+# Inspired by a response by 'Mital Vora' on Stack Overflow 
 
-                If an OSX user profile is detected it will back up:
-                    Desktop, Documents, Library, Movies, Music, and Pictures
+    if not exists( dest ):
+        mkdir( dest )
+    
+    for item in listdir( src ):
+        item_path = join( src, item )
+        dest_path = join( dest, item )
 
-                If a Windows user profile is detected it will back up:
-                    Desktop, Documents, Music, Pictures, and Videos''' )
-# end get_help
+        if isdir( item_path ):
+            backup( item_path, dest_path ) 
 
-def main():
+        else:
+            if not exists( dest_path ) or stat( item_path ).st_mtime - stat( dest_path ).st_mtime > 1:
+                try:
+                    print( "** Copying {} -> {}".format( item_path, dest_path ) )
+                    copy2( item_path, dest_path ) 
+                except OSError as error:
+                    print( "[ERROR] Failed to copy {}! Skiping! \n{}"
+                            .format( item_path, error ) )
+                    continue
+            else:
+                print( "** Skipping {}; It already exists and has not been updated since last backup"
+                        .format( item_path ) )
+        
+        
+def build_dirs( src ):
+# bulds a list of the folders that need to be backed up depending on your OS
 
-    if 3 != len( argv ): 
-        get_help()
-        exit()
+    if isdir( "Library" ):
+        # is OSX
+        print( "** OSX user profile detected" )
+        return [ "Desktop", "Documents", "Movies", "Music", "Pictures" ]
+    else:
+        # is Windows
+        print( "** Windows user profile detected" )
+        return [ "Desktop", "Documents", "Music", "Pictures", "Videos" ]
+# end build_dirs
 
-    src = argv[1]
-    dest = argv[2]
 
-    starting_dir = getcwd()
+def build_profiles( src ):
+# build a list of profiles to back up on multi-user systems
+    pass
 
+
+def check_input( src, mount, dest ):
+    ''' 
+    verifies that the source and destination directories exists.
+    If source is not a directory the script fails
+    If dest does not exist, it will attempt to create it and fails if it cant
+    '''
+    # verify source directory exists
     if not isdir( src ):
         print( "[ERROR] {} is not a directory \nThe script will now exit!".format( src ) )
         exit()
-    # verify existence of the destination dir and attempts to create is 
-    elif not isdir( dest ):
+
+    # verify that mount is a mount point
+    if not ismount( mount ):
+        print( "[ERROR] {} is not a mount point. The script will now exit!".format( mount ) )
+        exit()
+
+    # verify there is enough space on the mount point
+    check_space( src, mount )
+
+    # verify existence of the destination dir and attempts to create it
+    if not isdir( dest ):
         print( "[ERROR] {0} is not a directory. Attempting to make {0}".format( dest ) )
         try:
             mkdir( dest )
@@ -50,32 +91,96 @@ def main():
                     .format( dest, error ) )
             exit()
 
-    chdir( src )
-    print( "** Changed working directory to {}".format( src ) )
+# end check_input
 
-    if isdir( "Library" ):
-        # is OSX
-        dirs = [ "Desktop", "Documents", "Library", "Movies", "Music",
-                 "Pictures" ]
-        print( "** OSX user profile detected" )
+
+def check_space( src, mount ):
+# verifies that there is enough space on the destination
+        
+    ssize = popen( "du -sh " + src + "| awk '{print $1}'" ).read()[:-1] 
+    sfree = popen( "df -h | grep " + mount[:-1] + " | awk '{print $4}'" ).read()[:-1] 
+
+    size_bits = convert_to_bits( ssize )
+    free_bits = convert_to_bits( sfree )
+
+    if size_bits >= free_bits:
+        print( "[ERROR] there is not enough space on the destination: Src={} -> Dest={}"
+                .format( ssize, sfree ) )
     else:
-        # is Windows
-        dirs = [ "Desktop", "Documents", "Music", "Pictures", "Videos" ]
-        print( "** Windows user profile detected" )
+        print( "** Src size: {} -> Dest free: {}".format( ssize, sfree ) )
+        print( "** There is enough free space for the transfer." )
+# end check_space
 
-        for a_dir in dirs:
-            try:
-                if isdir( a_dir ):
-                    print( "** copying {0} -> {1}/{0}".format( a_dir, dest ) )
-                    copytree( a_dir , join( dest, a_dir ) )
-                else:
-                    print( "** Skipping {}, it is not a valid dir".format( a_dir ) )
-            except OSError as error:
-                print( "[ERROR] Failed to copy {0} \n{1} \nSkipping {0}".format( a_dir, error ) )
 
-    # return to the original working dir
-    chdir( starting_dir )
+def convert_to_bits( str_size ):
+# converts a string to an integer then determines it's size in bits
+    '''
+    could instead set up an enum that compares the last character and the 
+    number instead of doing a conversion. This might speed up the script
+    '''
+    unit = str_size[-1]
+    size = int( str_size[:-2] )
 
+    if unit == 'B':
+        size *= 8
+    elif unit == 'K':
+        size *= 8000
+    elif unit == 'M':
+        size *= 8000000
+    elif unit == 'G':
+        size *= 8000000000
+    elif unit == 'T':
+        size *= 8000000000000
+    else:
+        print( "[ERROR] The unit denoted by '{}', is not recognized. \n Exiting!"
+                .format( unit ) )
+        exit()
+
+    return size
+# end convert_to_bits
+
+
+def get_help():
+    print( '''\nThis script will back up user profile information.
+                Three parameters are passed in: 
+                first is the source directory usually the 'Users' directory
+                second is the destination mount point
+                third is the directory name where the files will be backedup 
+
+                Usage:
+                    backup.py <src_dir> <mount point> <dest_dir>
+
+                Example:
+                    backup.py "/mnt/Macintosh HD/Users" /mnt doej
+                    
+
+                If an OSX user profile is detected it will back up:
+                    Desktop, Documents, Movies, Music, and Pictures
+
+                If a Windows user profile is detected it will back up:
+                    Desktop, Documents, Music, Pictures, and Videos''' )
+# end get_help
+
+
+def main():
+
+    # argv - 0: backup.py 1: src directory 2: mount point 3: dest directory name
+    if 4 != len( argv ): 
+        get_help()
+        exit()
+
+    src = argv[1]
+    mount = argv[2]
+    dest = join( mount, "backups", argv[3] )
+
+    # verify directories, permissions, and if there is enough free space
+    check_input( src, mount, dest )
+
+    # build a list of the dirs that need to be backedup 
+    dirs = build_dirs( src )
+
+    # transfer the files from src -> dest
+    backup( src, dest )
 # end main
 
 
